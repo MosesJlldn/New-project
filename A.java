d96446c1d26b1c22342c6b0e78c37351fcd4cd71
MosesZ <mozas97@gmail.com>
2015-09-24 22:01:24 +0300

    new file

--- A.java
+++ A.java
@@ -1,9 +1,10 @@
 public class A {
 	public static void main (String args[]) {
 		double n = Double.parseDouble (args [0]);
+    double x = Double.parseDouble (args [1]);
 		double A=0; 
 		double y=0;
-		int i=1; int x=3;
+		int i=1;
 		for (i=1; i<=n; i++) {
 			A=Math.cos(x+A);
 		}
