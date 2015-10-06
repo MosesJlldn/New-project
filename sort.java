72c6a6793a92f044790b51ab648cca4e9064bb82
MosesZ <mozas97@gmail.com>
2015-10-06 22:49:56 +0300

    sort

--- sort.java
+++ sort.java
@@ -14,18 +14,24 @@ public class sort {
 	}	
 	public static void toSort(int[] a) {
 		int temp;
-		int step = a.length;
-		while (step >= 1) {
-			for (int j = 0; j < (a.length - step); j++) {
-				if (a[j] > a[j + step]) {
+		double factor = 1.247330950103979;
+		int step = (int)Math.round(a.length / factor);
+		boolean flag = true;
+		while ((step > 1)||(flag)) {
+			if (step > 1)
+				step = (int)(step / factor);
+			flag = false;
+			int j = 0;
+			while (j + step < a.length) {
+					if (a[j] > a[j + step]) {
 					temp = a[j];
 					a[j] = a[j + step];
 					a[j + step] = temp;
+					flag = true;
 				}
-				
+				j++;	
 			}
-			step = step-1;
-		}
+		}	
 	}
-
 }
+
